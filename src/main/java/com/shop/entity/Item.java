6f22af3173899d1fd91bc006d5fc3a99fa551224
Bokwang <qhrhkd550@naver.com>
2022-07-25 15:21:23 +0900

    상품 수정하기

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
@@ -41,4 +42,13 @@ public class Item extends BaseEntity{
 //    private LocalDateTime regTime;
 //
 //    private LocalDateTime updateTime;
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+
+    }
 }
